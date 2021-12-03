73f91bbabea17f9c3539095fb78c26e798217ad8
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2021-12-03 11:39:38 -0600

    used hook in checkout form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,30 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+export const useForm = () => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState({
+        firstName:"",
+        lastName:"",
+        address:"",
+        email:"",
+        city:"",
+        state:"",
+        zip:"",
+        notes:"",
+    });
+
+    const handleChanges = (input) => {
+        setValues({...values, [input.target.name]: input.target.value });
+    };
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit];
+
+}
+
+export default useForm;
